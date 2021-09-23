2f67e30ac62752d13ef09d3b9ede785752c55cef
AhmadRamadhan-bit <ahmadoneperson@gmail.com>
2021-09-24 06:24:43 +0700

    finish pages and router

---
+++ src/pages/Jadwal/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Jadwal = () => {
+    return (
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+            <Text>Jadwal Screen</Text>
+        </View>
+    )
+}
+
+export default Jadwal
+
+const styles = StyleSheet.create({})
